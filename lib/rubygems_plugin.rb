c791f670563573ebfc6ef91882201c284aca9b7c
TANIGUCHI Masaya <xtaniguchimasaya+code@gmail.com>
2018-12-30 01:00:50 +0900

    Update rubygems_plugin.rb

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 Gem.post_install do |installer|
   Dir.chdir "#{__dir__}/.." do
-    #system "bundle install --path=#{__dir__}/../vendor/bundle"
-    p `rake`
+    system "rake"
   end
 end
