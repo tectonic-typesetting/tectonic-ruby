a4d1be66483dcaeec87e7251f5af1d4cd74cf1a7
TANIGUCHI Masaya <ta2gch@gmail.com>
2018-12-31 01:45:01 +0900

    Remove origami

--- spec/tectonic_spec.rb
+++ spec/tectonic_spec.rb
@@ -1,6 +1,6 @@
 require "open3"
-require "origami"
-include Origami
+# require "origami"
+# include Origami
 RSpec.describe Tectonic do
   it "has a version number" do
     expect(Tectonic::VERSION).not_to be nil
@@ -21,16 +21,18 @@ RSpec.describe Tectonic do
     bin = pdf.pack "C*"
     File.binwrite "b.pdf", bin
     # Compare
-    zeros = sprintf("%032d", 0) # Dummy R/ID
-    a_pdf = PDF.read "a.pdf"
-    a_pdf.trailer.ID = [zeros, zeros] # Set dummy R/ID
-    a_pdf.save "a.pdf"
-    b_pdf = PDF.read "b.pdf"
-    b_pdf.trailer.ID = [zeros, zeros] # Set dummy R/ID
-    b_pdf.save "b.pdf"
+    #
     # Tectonic cannot generate same PDF
+    # zeros = sprintf("%032d", 0) # Dummy R/ID
+    # a_pdf = PDF.read "a.pdf"
+    # a_pdf.trailer.ID = [zeros, zeros] # Set dummy R/ID
+    # a_pdf.save "a.pdf"
+    # b_pdf = PDF.read "b.pdf"
+    # b_pdf.trailer.ID = [zeros, zeros] # Set dummy R/ID
+    # b_pdf.save "b.pdf"
     # cmp = FileUtils.cmp "a.pdf", "b.pdf" 
     # We compare two files as png
+    #
     system "gs", "-sDEVICE=png16m", "-o",  "a.png", "a.pdf"
     system "gs", "-sDEVICE=png16m", "-o",  "b.png", "b.pdf"
     cmp = FileUtils.cmp "a.png", "b.png" 
